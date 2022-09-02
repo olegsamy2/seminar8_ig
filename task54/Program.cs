588da5b313c7061d25cd2224fc2c2ac39ace87e3
OlegSam2 <doorsmania@mail.ru>
2022-09-02 19:17:42 +0300

    sort rows

---
+++ task54/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 54: Задайте двумерный массив. 	
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.	
+// Например, задан массив:	
+// 1 4 7 2	
+// 5 9 2 3	
+// 8 4 2 4	
+// В итоге получается вот такой массив:	
+// 7 4 2 1	
+// 9 5 3 2	
+// 8 4 4 2	
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void OrderLines(int[,] matrix)                          // сортировка на убывания в строках	
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)  // (-1) что б не вылетить за диапозон	
+            {
+                if (matrix[i, k] < matrix[i, k + 1])    // текущий элемент в строке сравнивается с последующим элементом в строке	
+                {
+                    int temp = matrix[i, k + 1];      // перестановка элементов в i(той) строке при выполнении условия	
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = FillMatrix(m, n);
+Console.WriteLine("Исходный массив: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+OrderLines(matrix);                    // вызов метода сортировки строк	
+Console.WriteLine("Отсортированный массив: ");
+PrintMatrix(matrix);
